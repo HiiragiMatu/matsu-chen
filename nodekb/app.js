81fc1ba99e8cc0586f05b11de72b1ea338333574
Matsu <B10523058@gemail.yuntech.edu.tw>
2020-07-09 17:00:30 +0800

    Day2 of building web

--- nodekb/app.js
+++ nodekb/app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+//Check for DB Connection
+db.once('open', function() {
+    console.log('Connected to MongoDB');
+});
+
+//Check for DB Errors
+db.on('error', function(err) {
+    console.log(err);
+});
+
 // Init App
 const app = express();
+
+// Bring in Models
+let Article = require('./models/articles');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -9,31 +27,16 @@ app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', function(req, res) {
-    let articles = [
-        {
-            id:1,
-            title: 'Article 1',
-            author: 'Matsu Chen',
-            body: 'This is article one'
-        },
-        {
-            id:2,
-            title: 'Article 2',
-            author: 'Matsu Chen',
-            body: 'This is article two'
-        },
-        {
-            id:3,
-            title: 'Article 3',
-            author: 'Matsu Chen',
-            body: 'This is article three'
-        },
-    ];
-    res.render('index', {
-        //Use npm install -g <name>, so that no need to restart
-        //tag: argument -> be passed to html 
-        title:'Articles',
-        articles: articles
+    Article.find({}, function(err, articles){
+        if(err) {
+            console.log(err);
+        }
+        res.render('index', {
+            //Use npm install -g <name>, so that no need to restart
+            //tag: argument -> be passed to html 
+            title:'Articles',
+            articles: articles
+        });
     });
 });
 

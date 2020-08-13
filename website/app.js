7e4045435b3c47a602810770507ebaffb41b84d2
Matsu <B10523058@gemail.yuntech.edu.tw>
2020-08-13 08:00:11 +0800

    upload

--- website/app.js
+++ website/app.js
@@ -1,8 +1,34 @@
-var http = require('http');
-var express = require('express');
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
 
 
+
+app.use(express.static('public'));
+/**
+ * Set directory to contain the templates('views')
+ * Set view engine to use, could be pug or other template format
+ */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+/**
+ * Default mongoose connection
+ * Bind connection to error event
+ */
+mongoose.connect('mongodb://localhost/website');
+let db = mongoose.connection;
+/**
+ * Check for DB connection and error
+ */
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+db.on('error', function(err) {
+  console.log(err);
+});
+
+const POST = process.env.PORT || 3000;
 /**
  * Router definition(callback func)
  */
@@ -10,7 +36,12 @@ app.get('/', function(req, res){
   res.send('Hello Matsu');
 });
 
-app.listen(3000, function(){
-  console.log('Listening on port 3000...');
+app.get('/about', function(req, res){
+  res.render('index', { title: 'Hey', msg: 'Where are you?'} )
 });
+
+app.listen(POST, function(){
+  console.log(`Listening on port ${POST}...`);
+});
+
 

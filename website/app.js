98975a81f446763fb906541d81824969454f5839
Matsu <B10523058@gemail.yuntech.edu.tw>
2020-08-13 16:55:12 +0800

    upload

--- website/app.js
+++ website/app.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const MongoClient = require('mongodb').MongoClient;
-const app = express.Router();
+const app = express();
 const path = require('path');
+const createError = require('http-errors');
+const logger = require('morgan');
 
+/**
+ * Require modules created by myself
+ */
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
 
-
+/**
+ * Specify modules that are going to be used
+ */
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
+/**
+ * When catch 404 error, forward to error handler
+ */
+app.use(function(req, res, next){
+  next(createError(404));
+});
+app.use(function(err, req, res, next){
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
 /**
  * Set directory to contain the templates('views')
  * Set view engine to use, could be pug or other template format
@@ -34,17 +61,19 @@ const POST = process.env.PORT || 3000;
  * Router definition(callback func)
  */
 app.get('/', function(req, res){
-  res.send('Hello Matsu');
+  res.render('index');
 });
 
 app.get('/about', function(req, res){
-  res.render('index', { title: 'Hey', msg: 'Where are you?'} )
+  res.render('about');
 });
+
+app.get('/porfolio', function(req, res){
+  res.render('portfolio');
+})
 
 app.listen(POST, function(){
   console.log(`Listening on port ${POST}...`);
 });
 
-
-module.exports = router;
 module.exports = app;
